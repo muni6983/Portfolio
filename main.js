086aa004ca486981c867cdabbab81de4f2bdf3b7
gyeongmun <bada6983@gmail.com>
2021-11-23 14:04:25 +0900

    Handle scrolling when tapping on the navbar menu

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 "use strict";
 
 //scroll -> change navbar background color
-const navbar = document.querySelector("nav");
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", (event) => {
   let scrollPosition = window.scrollY;
-  const navbarHeight = navbar.getBoundingClientRect().height;
   if (scrollPosition >= navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add("navbar--pink");
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove("navbar--pink");
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (!link) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth", block: "start" });
 });
